e11205275bc30e68115787747bd12ba4a137e8f9
krutik228 <nikkrutik@mail.ru>
2021-02-24 23:13:52 +0700

    Выполнено PageObject преобразование: все ассерты в теле тестов убраны, все проверки реализованы в отдельные методы

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,9 @@ import pytest
 import time
 
 
+link_on_the_product = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
 @pytest.mark.need_review
 @pytest.mark.parametrize('link', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8",
                                   "9"])
@@ -15,38 +18,33 @@ def test_guest_can_add_product_to_basket(browser, link):
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_on_the_product)
     page.add_product_to_basket()
-    page.guest_cant_see_success_message(link)
+    page.guest_cant_see_success_message(link_on_the_product)
 
 
 def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
-    page.guest_cant_see_success_message(link)
+    page = ProductPage(browser, link_on_the_product)
+    page.guest_cant_see_success_message(link_on_the_product)
 
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_on_the_product)
     page.open()
     page.add_product_to_basket()
-    page.message_disappeared_after_adding_product_to_basket(link)
+    page.message_disappeared_after_adding_product_to_basket(link_on_the_product)
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_on_the_product)
     page.open()
     page.should_be_login_link()
 
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/"
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, link_on_the_product)
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
@@ -56,7 +54,6 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/"
     page = ProductPage(browser, link)
-    time.sleep(5)
     page.guest_cant_see_product_in_basket_opened_from_product_page(browser, link)
 
 
@@ -79,12 +76,10 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_authorized_user()
 
     def test_user_cant_see_success_message(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
-        page = ProductPage(browser, link)
-        page.user_cant_see_success_message(browser, link)
+        page = ProductPage(browser, link_on_the_product)
+        page.user_cant_see_success_message(browser, link_on_the_product)
 
     @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/hacking-exposed-wireless_208/"
-        page = ProductPage(browser, link)
-        page.user_can_add_product_to_basket(browser, link)
+        page = ProductPage(browser, link_on_the_product)
+        page.user_can_add_product_to_basket(browser, link_on_the_product)
